94030589265e27f7bada6282cd2bee780ea9aa96
jocex <82776763+jocex@users.noreply.github.com>
2021-11-15 01:59:48 -0500

    Changed Path System

--- Assets/Week8/003/Scripts/GoodScore.cs
+++ Assets/Week8/003/Scripts/GoodScore.cs
@@ -6,11 +6,19 @@ public class GoodScore : MonoBehaviour
 {
     CircleCollider2D GoodCircle;
     bool inGood;
+    public AudioClip Ding;
+    AudioSource audioSource;
+    
    
 
-    private void Awake(){
+   private void Awake(){
         GoodCircle=GetComponent<CircleCollider2D>();
         GoodCircle.isTrigger = true;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start(){
+        //audioSource = GetComponent<AudioSource>();
     }
 
       public void OpenGood(){
@@ -22,6 +30,7 @@ public class GoodScore : MonoBehaviour
    void Update(){
 
         if(inGood == true){
+              audioSource.PlayOneShot(Ding,1);
              OpenGood();
              ScoreManager.AddToScore();
          }
@@ -30,19 +39,23 @@ public class GoodScore : MonoBehaviour
 
     
 
-    private void OnTriggerEnter2D(Collider2D other){
+     private void OnTriggerEnter2D(Collider2D other){
         if( other.CompareTag("Player")){
             inGood = true;
+             //if(!Ding.isPlaying){
+               
+              
+           // }
             
            
            
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other){
+   /* private void OnTriggerExit2D(Collider2D other){
         if( other.CompareTag("Player")){
         inGood = false;
         }
-    }
+    }*/
        
 }
--- Assets/Week8/003/Scripts/PathSystem.cs
+++ Assets/Week8/003/Scripts/PathSystem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
 public class PathSystem : MonoBehaviour {
 
     public enum SeedType { RANDOM, CUSTOM }
@@ -14,12 +15,19 @@ public class PathSystem : MonoBehaviour {
     [Space]
     public bool animatedPath;
     public List<MyGridCell> gridCellList = new List<MyGridCell>();
+    public List<MyGridCell> UpBorderList = new List<MyGridCell>();
+    public List<MyGridCell> DownBorderList = new List<MyGridCell>();
+    
     public int pathLength = 10;
     [Range(1.0f, 10.0f)]
     public float cellSize = 1.0f;
+    public float offset = 1.0f;
 
     public Transform startLocation;
+    public Transform UpLocation;
+    public Transform DownLocation;
     public GameObject mazePrefab;
+    public GameObject maze2Prefab;
     public GameObject[] powerups;
 
     // Start is called before the first frame update
@@ -51,31 +59,79 @@ public class PathSystem : MonoBehaviour {
 
     void CreatePath() {
 
+        UpBorderList.Clear();
+        DownBorderList.Clear();
         gridCellList.Clear();
         Vector2 currentPosition = startLocation.transform.position;
+        Vector2 UpPosition = UpLocation.transform.position;
+        Vector2 DownPosition = DownLocation.transform.position;
         gridCellList.Add(new MyGridCell(currentPosition));
+        UpBorderList.Add(new MyGridCell(UpPosition));
+        DownBorderList.Add(new MyGridCell(DownPosition));
 
-        for (int i = 0; i < pathLength; i++) {
+        //Original path
+        for (int i = 0; i < 10; i++) {
 
             int n = random.Next(100);
 
             if (n.IsBetween(0, 49)) {
                 currentPosition = new Vector2(currentPosition.x + cellSize, currentPosition.y);
+                UpPosition = new Vector2(UpPosition.x + cellSize, UpPosition.y );
+                DownPosition = new Vector2(DownPosition.x + cellSize, DownPosition.y);
+
             }
             else {
                 currentPosition = new Vector2(currentPosition.x, currentPosition.y + cellSize);
+                UpPosition = new Vector2(UpPosition.x , UpPosition.y + cellSize);
+                DownPosition = new Vector2(DownPosition.x , DownPosition.y + cellSize);
             }
 
-            gridCellList.Add(new MyGridCell(currentPosition));
-            
-             GameObject m = Instantiate(mazePrefab,gridCellList[i].location, Quaternion.identity);
-             
+             gridCellList.Add(new MyGridCell(currentPosition));  
+             UpBorderList.Add(new MyGridCell(UpPosition));
+             DownBorderList.Add(new MyGridCell(DownPosition));
 
+             GameObject r = Instantiate(mazePrefab,gridCellList[i].location, Quaternion.identity);
+             GameObject m = Instantiate(maze2Prefab,UpBorderList[i].location, Quaternion.identity);
+             GameObject o = Instantiate(maze2Prefab,DownBorderList[i].location,Quaternion.identity);
         }
+        //powerups
         for (int j=0; j<5; j++){
   
             GameObject p = Instantiate(powerups[Random.Range(0,2)],gridCellList[Random.Range(2,10)].location,Quaternion.identity);
         }
+
+        //UpBorder
+       /* for (int k = 0; k < pathLength; k++){
+
+            int o = random.Next(100);
+            
+            if (o.IsBetween(0,49)){
+                UpPosition = new Vector2(UpPosition.x + cellSize, UpPosition.y);
+            }
+            else{
+                UpPosition = new Vector2(UpPosition.x, UpPosition.y +cellSize);
+            }
+
+            UpBorderList.Add(new MyGridCell(UpPosition));
+            GameObject m = Instantiate(mazePrefab,gridCellList[k].location , Quaternion.identity);
+            
+        }
+
+        for (int l = 0; l < pathLength; l++){
+
+            int q = random.Next(100);
+               
+            if (q.IsBetween(0,49)){
+                DownPosition = new Vector2(DownPosition.x + cellSize, DownPosition.y);
+            }
+            else{
+                DownPosition = new Vector2(DownPosition.x, DownPosition.y +cellSize);
+            }
+
+            DownBorderList.Add(new MyGridCell(DownPosition));
+            GameObject m = Instantiate(mazePrefab,gridCellList[l].location, Quaternion.identity);
+
+        }*/
     }
   
     IEnumerator CreatePathRoutine() {
@@ -84,7 +140,7 @@ public class PathSystem : MonoBehaviour {
         Vector2 currentPosition = startLocation.transform.position;
         gridCellList.Add(new MyGridCell(currentPosition));
 
-        for (int i = 0; i < pathLength; i++) {
+        for (int i = 0; i < 10; i++) {
 
             int n = random.Next(100);
 
